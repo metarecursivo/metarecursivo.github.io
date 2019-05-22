d498af3f455d432f40d091a067a2c22ecc8c9fa8
metarecursivo <conrderecursivo@gmail.com>
2019-05-22 20:20:31 +0200

    Revert "prueba js"
    This reverts commit c0a2c709a07204f0059e57be79e882bf19b6be40.

--- sekiro/js/main.js
+++ sekiro/js/main.js
@@ -44,15 +44,14 @@ var profilesKey = 'darksouls3_profiles';
 
         populateProfiles();
 
-        $('input[type="checkbox"]').click(function() {
+        $('.checkbox input[type="checkbox"]').click(function() {
             var id = $(this).attr('id');
             var isChecked = profiles[profilesKey][profiles.current].checklistData[id] = $(this).prop('checked');
-            //_gaq.push(['_trackEvent', 'Checkbox', (isChecked ? 'Check' : 'Uncheck'), id]);
-            $(this).parent().parent().find('li > label > input[type="checkbox"]').each(function() {
-                var id = $(this).attr('id');
-                profiles[profilesKey][profiles.current].checklistData[id] = isChecked;
-                $(this).prop('checked', isChecked);
-            });
+            if (isChecked === true) {
+              $('[data-id="'+id+'"] label').addClass('completed');
+            } else {
+              $('[data-id="'+id+'"] label').removeClass('completed');
+            }
             $.jStorage.set(profilesKey, profiles);
             calculateTotals();
         });
